f27aff0252924a434ff88b7935427317dff6bc8b
Simon Trény <streny@betomorrow.com>
2019-03-11 16:48:56 +0100

    Actually animate header views to implement parallax effect

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Animated, Image, Text, View } from "react-native";
+import { Animated, StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   state = { text: "", scrollOffset: new Animated.Value(0) };
@@ -22,19 +22,84 @@ export default class App extends React.Component {
   }
 
   renderHeader() {
-    const headerHeight = 240;
+    const { scrollOffset } = this.state;
+    const expandedHeaderHeight = 240;
+    const collapsedHeaderHeight = 64;
+    const titleHeight = 44;
+    const scrollSpan = expandedHeaderHeight - collapsedHeaderHeight;
+
     return (
       <Animated.View
         style={{
-          height: headerHeight,
-          transform: [{ translateY: this.state.scrollOffset }],
+          height: expandedHeaderHeight,
           zIndex: 100,
+          overflow: "hidden",
+          transform: [
+            {
+              translateY: Animated.subtract(
+                scrollOffset,
+                scrollOffset.interpolate({
+                  inputRange: [0, scrollSpan],
+                  outputRange: [0, scrollSpan],
+                  extrapolate: "clamp",
+                })
+              ),
+            },
+          ],
         }}
       >
-        <Image
-          style={[{ width: "100%", height: "100%" }]}
+        <Animated.Image
+          style={{
+            width: "100%",
+            height: "100%",
+            transform: [
+              {
+                translateY: scrollOffset.interpolate({
+                  inputRange: [0, scrollSpan],
+                  outputRange: [0, scrollSpan / 2],
+                  extrapolate: "clamp",
+                }),
+              },
+            ],
+          }}
           source={require("./cover.jpg")}
         />
+        <Animated.View
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              backgroundColor: "black",
+              opacity: scrollOffset.interpolate({
+                inputRange: [scrollSpan / 2, scrollSpan],
+                outputRange: [0, 0.85],
+                extrapolate: "clamp",
+              }),
+            },
+          ]}
+        />
+        <Animated.Text
+          style={{
+            position: "absolute",
+            left: 0,
+            right: 0,
+            bottom: 16,
+            fontSize: 16,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: "white",
+            transform: [
+              {
+                translateY: scrollOffset.interpolate({
+                  inputRange: [scrollSpan, scrollSpan + titleHeight],
+                  outputRange: [titleHeight, 0],
+                  extrapolate: "clamp",
+                }),
+              },
+            ],
+          }}
+        >
+          BORDEAUX
+        </Animated.Text>
       </Animated.View>
     );
   }
